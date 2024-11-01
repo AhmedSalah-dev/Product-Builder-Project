60d31d66a051cca718d9f50ebf5320285b07759d
AhmedSalah-dev <Midoelshal78@gmail.com>
2024-11-01 14:38:00 +0200

    fix a bug

--- src/compnents/ui/Model.tsx
+++ src/compnents/ui/Model.tsx
@@ -21,16 +21,16 @@ const Model= ({isOpen,closeModel: close,title,children,description}:IProps) => {
       <div className="fixed inset-0 backdrop-blur-sm" aria-hidden="true" />
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4">
-          <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-lg bg-white p-6 text-left align-middle shadow-xl transition-all">
+          <DialogPanel className="w-full max-w-md transform overflow-hidden rounded-lg bg-white p-6 text-left align-middle shadow-xl transition-all">
                   {title && (
-                    <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                    <DialogTitle as="h3" className="text-lg font-medium leading-6 text-gray-900">
                       {title}
-                    </Dialog.Title>
+                    </DialogTitle>
                   )}
                   {description && <p className="text-sm text-gray-500 mt-3">{description}</p>}
 
                   <div className="mt-4">{children}</div>
-                </Dialog.Panel>
+                </DialogPanel>
           </div>
         </div>
       </Dialog>
